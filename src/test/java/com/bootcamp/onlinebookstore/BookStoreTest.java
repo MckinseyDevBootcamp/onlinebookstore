57ebdb9d159fc9f5608b70615f7f196aa7a8452a
suyash kumar singh <singsuyash@gmail.com>
2023-07-07 15:11:39 +0530

    Deliberately fail test for demo

--- src/test/java/com/bootcamp/onlinebookstore/BookStoreTest.java
+++ src/test/java/com/bootcamp/onlinebookstore/BookStoreTest.java
@@ -2,7 +2,7 @@ package com.bootcamp.onlinebookstore;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 public class BookStoreTest {
     @Test
@@ -11,6 +11,6 @@ public class BookStoreTest {
         Book book = new Book();
         Boolean isBookIssued = bookStore.issue(book);
 
-        assertTrue(isBookIssued);
+        assertFalse(isBookIssued);
     }
 }
